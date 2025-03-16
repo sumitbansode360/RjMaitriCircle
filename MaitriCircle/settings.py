13b54b1793d50e40fa8ec35adbe0f1135c64f00b
sumitbansode360 <sumitbansode360@gmail.com>
2025-03-16 13:29:14 +0530

    env set

--- MaitriCircle/settings.py
+++ MaitriCircle/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from environ import Env
+env = Env()
+env.read_env()
+
+ENVIRONMENT = env('ENVIRONMENT', default='production')
+ENVIRONMENT = "production"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +27,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dwsr51&jaxxlig9r3^_5tr1i2a3@7oeyms1$d(stkdr@#8#pxj'
+# SECRET_KEY = 'django-insecure-dwsr51&jaxxlig9r3^_5tr1i2a3@7oeyms1$d(stkdr@#8#pxj'
+
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if ENVIRONMENT == 'development':
+    DEBUG = True
+else:
+    DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
